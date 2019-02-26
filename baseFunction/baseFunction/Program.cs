90c92210fa588ff66c62bff78d7850e688be891a
TPal71 <treso.pal@trexsys.hu>
2019-02-26 13:15:24 +0100

    Modify code

--- baseFunction/baseFunction/Program.cs
+++ baseFunction/baseFunction/Program.cs
@@ -10,6 +10,34 @@ namespace baseFunction
     {
         static void Main(string[] args)
         {
+
+            //Függvény meghívása
+            Console.WriteLine(baseAdd(5, 6));
+            Console.WriteLine(baseMine(18, 6));
+            Console.WriteLine(baseMul(18, 6));
+
+
+            Console.ReadKey();
         }
+        //Függvény megvalósítása
+        static int baseAdd(int a, int b)
+        {
+            // int c = a + b;
+            return a + b;
+        }
+        //Függvény megvalósítása
+        static int baseMine(int a, int b)
+        {
+            int c = a - b;
+            return c;
+        }
+        //Függvény megvalósítása
+        static int baseMul(int a, int b)
+        {
+            int c = a * b;
+            return c;
+        }
+
     }
+}
 }
